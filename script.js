51c505b016e83861a867f14e569eaf773a1a1fb6
matheus-ma1a <matheussantosmaia@gmail.com>
2021-10-07 17:32:08 -0300

    cria o css da pagina

--- script.js
+++ script.js
@@ -3,9 +3,3 @@ const input = document.querySelector(".input")
 
 const numbers = document.querySelector(".numbers")
 
-
-input.addEventListener("input", (keydown) => {
-    
-    numbers.innerHTML = input.value.length
-    
-})
